1b79c8c275266ad3e98f83bfe0ae3e8c4c9aa7b9
Matthew Liu <96169489+TheMattBin@users.noreply.github.com>
2024-10-12 13:48:22 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const mapConfig = {
   // replace your own voxel layer id here
-  "web-scene-id": "a75e9f6c502e4cbabfb74c30a03801d9",
+  // "web-scene-id": "a75e9f6c502e4cbabfb74c30a03801d9",
+  "web-scene-id": "cb511f5eba23446eb668a324e844ee80",
 };
 
 // change name and id based on your own voxel layer
